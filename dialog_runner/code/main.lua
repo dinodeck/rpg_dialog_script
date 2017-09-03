89a63cf7d1b2a26f20110dc6c837fc616ca5783a
Dan Schuller <dan@godpatterns.com>
2017-09-03 12:22:05 +0100

    Added an extra test case

--- dialog_runner/code/main.lua
+++ dialog_runner/code/main.lua
@@ -6,7 +6,7 @@ Asset.Run('PlayController.lua')
 
 gRenderer = Renderer.Create()
 gFont = BitmapText:Create(DefaultFontDef)
-gPath = "example_5.txt"
+gPath = "example_6.txt"
 gIndicator = Sprite:Create()
 gIndicator:SetTexture(Texture.Find("indicator.png"))
 
